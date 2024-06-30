11ae3a8455711511adb538d49aa90c5f41e2502c
anas <anas3ez@gmail.com>
2024-06-30 20:22:25 +0300

    splash_screen

---
+++ lib/features/splash_screen/splashScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
+  }
+}
