b96df5d6fc6e5063427996899624671ab2371888
anas <anas3ez@gmail.com>
2024-07-04 10:44:14 +0300

    set_up_setting_screen

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -6,10 +6,11 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Title'),
-      ),
-      body: Container(),
-    );
+        appBar: AppBar(
+          title: const Text('Title'),
+        ),
+        body: const Column(
+          children: [Text('settings_screen')],
+        ));
   }
 }
